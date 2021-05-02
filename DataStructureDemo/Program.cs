faabe70956decd9f46a122911e92b3fc2cba5566
pbishal <pbishal60@gmail.com>
2021-05-02 20:07:24 +0530

    Linked list

--- DataStructureDemo/Program.cs
+++ DataStructureDemo/Program.cs
@@ -10,7 +10,31 @@ namespace DataStructureDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to data structure");
+            Console.WriteLine("==============  LINKED LIST DATA STRUCTURE  =================");
+
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+
+            Console.WriteLine("\nINSERT AT PERTICULAR POSITION");
+            linkedList.InsertAtParticularPosition(2, 100);
+            linkedList.InsertAtParticularPosition(3, 300);
+            linkedList.InsertAtParticularPosition(4, 400);
+            linkedList.Display();
+
+            linkedList.RemoveFirstNode();
+            linkedList.Display();
+
+            linkedList.RemovaLastNode();
+            linkedList.Display();
+
+            linkedList.Search(30);
+            linkedList.Display();
+
+            Console.ReadKey();
         }
     }
 }
